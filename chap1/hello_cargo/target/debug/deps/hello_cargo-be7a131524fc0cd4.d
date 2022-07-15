68c482f6afdc17dc694f39770728700d1df083c3
david <divadnoslo@gmail.com>
2022-07-15 17:05:46 -0600

    Chapter 1 Complete with Cargo

---
+++ chap1/hello_cargo/target/debug/deps/hello_cargo-be7a131524fc0cd4.d
@@ -0,0 +1,5 @@
+/home/david/Desktop/rust-practice/chap1/hello_cargo/target/debug/deps/hello_cargo-be7a131524fc0cd4: src/main.rs
+
+/home/david/Desktop/rust-practice/chap1/hello_cargo/target/debug/deps/hello_cargo-be7a131524fc0cd4.d: src/main.rs
+
+src/main.rs:
